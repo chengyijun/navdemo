d07244dee8dff3f214633a40b4ebe2753f6d6bbb
chengyijun <cyjmmy@foxmail.com>
2020-12-02 17:18:25 +0800

    tabbar插槽封装

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,41 @@
 <template>
     <div id="app">
         <router-view></router-view>
-        <tab-bar></tab-bar>
+        <tab-bar>
+            <tab-bar-item path="/index">
+                <img slot="img" src="../public/imgs/index.png" alt="首页">
+                <img slot="img-active" src="../public/imgs/index_selected.png" alt="首页">
+                <p slot="title">首页</p>
+            </tab-bar-item>
+            <tab-bar-item path="/sale">
+                <img slot="img" src="../public/imgs/sale.png" alt="热卖">
+                <img slot="img-active" src="../public/imgs/sale-selected.png" alt="热卖">
+                <p slot="title">热卖</p>
+            </tab-bar-item>
+            <tab-bar-item path="/buy">
+                <img slot="img" src="../public/imgs/buy.png" alt="购物车">
+                <img slot="img-active" src="../public/imgs/buy-selected.png" alt="购物车">
+                <p slot="title">购物车</p>
+            </tab-bar-item>
+            <tab-bar-item path="/mine">
+                <img slot="img" src="../public/imgs/mine.png" alt="我的">
+                <img slot="img-active" src="../public/imgs/mine-selected.png" alt="我的">
+                <p slot="title">我的</p>
+            </tab-bar-item>
+        </tab-bar>
     </div>
 </template>
 
 
 <script>
     import TabBar from "./components/TabBar";
+    import TabBarItem from "./components/TabBarItem";
 
     export default {
         name: 'App',
         components: {
-            TabBar
+            TabBar,
+            TabBarItem
         }
     }
 </script>
--- src/components/TabBarItem.vue
+++ src/components/TabBarItem.vue
@@ -1,15 +1,83 @@
 <template>
-    <div>
 
+    <div class="tabbar-item" @click="clickHandle">
+        <div v-show="!isActive">
+            <slot name="img"></slot>
+        </div>
+        <div v-show="isActive">
+            <slot name="img-active"></slot>
+        </div>
+        <div :class="{active: isActive, title: true}">
+            <slot name="title"></slot>
+        </div>
     </div>
 </template>
 
 <script>
+
     export default {
-        name: "TabBarItem"
+        name: "TabBarItem",
+        data() {
+            return {
+                // isActive: false
+            }
+        },
+        computed: {
+            isActive: {
+                get() {
+                    if (this.$route.matched.length > 0)
+                        return this.$route.matched[0].path === this.path
+                    return false
+                },
+                set() {
+
+                }
+            }
+        },
+        props: {
+            path: {
+                type: String,
+                default() {
+                    return ''
+                }
+            }
+        },
+        methods: {
+            clickHandle() {
+                // 路由跳转
+                if (this.$route.matched[0].path !== this.path)
+                    this.$router.replace(this.path)
+            }
+        },
+        beforeRouteLeave(to, from, next) {
+            console.log(11111);
+            next()
+        }
     }
 </script>
 
-<style scoped>
+<style>
+    .tabbar-item {
+        flex: 1;
+        text-align: center;
+    }
 
+
+    .tabbar-item img {
+        width: 29px;
+        height: 29px;
+    }
+
+    .title {
+        font-size: 16px;
+        position: relative;
+        top: -5px;
+    }
+
+    .title.active {
+        font-size: 16px;
+        position: relative;
+        top: -5px;
+        color: #1AFA29;
+    }
 </style>
